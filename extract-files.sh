b0bc6cbd398df91583260f68ce725864a73a97e5
EreN <akbaseren4751@gmail.com>
2023-01-25 06:26:35 +0800

    rosemary: Fix android.hardware.thermal@2.0-service.mtk crashing
    Fixes: https://gist.githubusercontent.com/MrErenK/2355a2df7b04e9b3f6e4cf4752ef4518/raw/4d070b9dcf4b9f80ee6c4c412a457516feb3a0d8/thermals.txt
    
    Co-authored-by: johnmart19 <johnivan19999@gmail.com>
    Signed-off-by: CodeChas3r1 <codechas3r@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -71,6 +71,9 @@ function blob_fixup {
         vendor/lib*/hw/dfps.mt6785.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
+        vendor/bin/hw/android.hardware.thermal@2.0-service.mtk)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+            ;;
         vendor/lib/libMtkOmxVdecEx.so)
             "${PATCHELF}" --replace-needed "libui.so" "libui-v32.so" "${2}"
             ;;
